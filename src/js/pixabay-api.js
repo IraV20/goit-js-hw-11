a2a4c0bc476858fafd06df13c61bf4881d357966
Iryna Voziyan <iryna221020@gmail.com>
2024-03-26 18:30:48 +0200

    ready for check

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,16 @@
+export default function searchImg(userValue) {
+  const BASE_URL = 'https://pixabay.com';
+  const END_POINT = '/api/';
+  const API_KEY = '42716405-8ee00579c2dae2c7c3ba8a313';
+  const PARAMS = `?key=${API_KEY}&q=${userValue}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+  const url = BASE_URL + END_POINT + PARAMS;
+
+  return fetch(url).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error(res.status);
+    }
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,43 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import searchImg from './js/pixabay-api';
+import renderImg from './js/render-functions';
+import { imgList } from './js/render-functions';
+
+const form = document.querySelector('form');
+const loader = document.querySelector('.loader');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  imgList.innerHTML = '';
+
+  const userValue = e.target.elements.search.value;
+  loader.classList.remove('is-hidden');
+
+  searchImg(userValue).then(images => {
+    if (images.hits.length === 0) {
+      validImg(images);
+    } else {
+      renderImg(images);
+    }
+    loader.classList.add('is-hidden');
+  });
+
+  e.target.reset();
+});
+
+function validImg(images) {
+  iziToast.error({
+    title:
+      'Sorry, there are no images matching your search query. Please try again!',
+    position: 'topRight',
+    titleColor: '#fff',
+    backgroundColor: '#EF4040',
+    titleSize: '16px',
+    titleLineHeight: '1.5',
+    close: true,
+    icon: '',
+  });
+}
